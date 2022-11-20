f162c7ecf5b6d8b148df6ebdb15fc82e150f74e1
Starforcraft <starforcraft@yahoo.com>
2022-11-20 18:07:01 +0100

    Fixed advancement being grant when joining world

--- src/main/java/com/YTrollman/CreativeWirelessTransmitter/container/CreativeWirelessTransmitterContainerMenu.java
+++ src/main/java/com/YTrollman/CreativeWirelessTransmitter/container/CreativeWirelessTransmitterContainerMenu.java
@@ -6,6 +6,7 @@ import com.refinedmods.refinedstorage.container.BaseContainerMenu;
 import net.minecraft.world.entity.player.Player;
 
 public class CreativeWirelessTransmitterContainerMenu extends BaseContainerMenu {
+
     public CreativeWirelessTransmitterContainerMenu(CreativeWirelessTransmitterBlockEntity wirelessTransmitter, Player player, int windowId) {
         super(ModContainers.CREATIVE_WIRELESS_TRANSMITTER, wirelessTransmitter, player, windowId);
     	
